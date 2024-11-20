96ae5d410841dc78e35c12d554dc2a192ba1decc
ThatGhost <stargamer.me@gmail.com>
2024-11-20 20:26:24 +0100

    added material and updated the basic info page

--- src/Components/Pages/Start/Start-FirstPage/Start-FirstPage.component.ts
+++ src/Components/Pages/Start/Start-FirstPage/Start-FirstPage.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { AppService } from '../../../../Services/Header.service';
 import { ButtonRoundComponent } from '../../../Standalone/Button-Round/Button-Round.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-Start-FirstPage',
   templateUrl: './Start-FirstPage.component.html',
   styleUrls: ['./Start-FirstPage.component.scss'],
-  imports: [ButtonRoundComponent]
+  imports: [ButtonRoundComponent, MatFormFieldModule, MatInputModule, MatSelectModule, CommonModule]
 })
 export class StartFirstPageComponent {
+  public ageIsNumber: boolean | null = null; 
 
   constructor(private readonly appService: AppService) {
   }
@@ -19,4 +24,18 @@ export class StartFirstPageComponent {
     this.appService.PlayAnimation(true);
     this.appService.ChangeHeaderName("Basic information");
   }
+
+  public OnNext(name: string, country: string, age: string, status: string) {
+    if(!this.CanClickNext(name, country, age, status)) return;
+  }
+
+  public CanClickNext(name: string, country: string, age: string, status: string): boolean {
+    return !!name && !!country && !!age && !!status && this.ageIsNumber === true;
+  }
+
+  public IsNumber(numb: string) {
+    this.ageIsNumber = !isNaN(parseInt(numb));
+    console.log(this.ageIsNumber);
+  }
+
 }
