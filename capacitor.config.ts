ba8e523193d547763cbd98502d675cf184200f35
ThatGhost <stargamer.me@gmail.com>
2024-11-21 13:22:39 +0100

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.budgetsim.app',
+  appName: 'budget-simulator',
+  webDir: 'www'
+};
+
+export default config;
